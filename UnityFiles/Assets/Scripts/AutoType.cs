f63e16af93d2bcc878ffb8867fbd14ce0af8907a
Nikko Williard <nikko.williard@gmail.com>
2014-09-21 03:56:31 -0400

    Added AutoType.cs to draw the GUI box and display the text in a typewriter fashion. frame.cs grabs the dialoge.xml and turns it into a Game object.

---
+++ UnityFiles/Assets/Scripts/AutoType.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//from http://wiki.unity3d.com/index.php/AutoType
+public class AutoType : MonoBehaviour {
+	
+	public float letterPause = 0.01f;
+	public AudioClip sound;
+	public List<Frame> list = new List<Frame> ();
+	public Game data = new Game();
+	string message = "";
+	// Use this for initialization
+	void Start () {
+		string toDisplay = "Whatever data is being sent, be it cat pictures, live video, " +
+		"or life-saving tools about water filtration, all this data is subjected" +
+			"to Internet Service Providers whims without Net Neutrality. Net Neutrality" +
+				"keeps Internet Service Provider fair and balanced about their data transfer. " +
+				"Without Net Neutrality, cat pictures online could be eradicated. ";
+		data = loadFrames("Assets/XML/dialogue.xml");
+		foreach(Frame f in data.getFrames){
+			list.Add(f);
+		}
+		Debug.Log(list.Count);
+		StartCoroutine (TypeText ());
+	}
+	//though a function for the Game class
+	//this is kept outside to be used in unity runtime
+	Game loadFrames(string xmlFile){
+		System.Xml.Serialization.XmlSerializer serializer = 
+			new System.Xml.Serialization.XmlSerializer(typeof(Game));
+		System.IO.TextReader reader = 
+			new System.IO.StreamReader(xmlFile);
+		Game ObjItems = (Game)serializer.Deserialize(reader);
+		reader.Close();
+		return ObjItems;
+	}
+	
+
+	
+	// Update is called once per frame
+	//void Update (){
+
+	//}
+
+	//Draws the text box
+	void OnGUI() {
+		FontStyle bold;
+		GUI.skin.box.wordWrap = true;
+		GUI.Box(new Rect(Screen.width - (Screen.width-5), 3*(Screen.height/4)-5, Screen.width - 10, Screen.height/4), message);
+	}
+
+	IEnumerator TypeText () {
+		Debug.Log ("TypeText: Called");
+		//this.gameObject = f.sprite;
+		foreach (Frame f in data.getFrames) 
+		{
+			foreach (char letter in f.text.ToCharArray()) 
+			{
+				message += letter;
+				if (sound)
+						audio.PlayOneShot (sound);
+				//Debug.Log ("TypeText: Message - " + message);
+				yield return 0;
+				yield return new WaitForSeconds (letterPause);
+			}
+		}
+	}
+}
---
+++ UnityFiles/Assets/Scripts/frame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+
+public class Frame {
+
+	[XmlElement(ElementName = "sprite")]
+	public string spriteName { get; set; }
+
+	[XmlElement(ElementName = "text")]
+	public string text { get; set; }
+
+	public Sprite sprite {
+		get { 
+			return Resources.Load(this.spriteName, typeof(Sprite)) as Sprite;
+		}
+	}
+}
+
+[XmlRootAttribute("game")]
+public class Game {
+	
+	[XmlArray("frames")]
+	[XmlArrayItem("frame", Type = typeof(Frame))]
+	public Frame[] getFrames;
+
+
+}
