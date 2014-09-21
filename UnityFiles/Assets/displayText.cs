d94fc3b71dfbac4bb732f5c1e188ab4fc18eaf7e
Nikko Williard <nikko.williard@gmail.com>
2014-09-20 21:05:46 -0400

    Added drawBox.cs and displayText.cs. Saved as GameGameGame.unity

---
+++ UnityFiles/Assets/displayText.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class displayText : MonoBehaviour {
+
+	public string textShownOnScreen;
+	public string fullText = "The text you want shown on screen with typewriter effect.";
+	public float wordsPerSecond = 2; // speed of typewriter
+	private float timeElapsed = 0;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		textShownOnScreen = GetWords(fullText, timeElapsed * wordsPerSecond);
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(5, 440, Screen.width - 10, Screen.height/4), "");
+	}
+	
+	private string GetWords(string text, int wordCount)
+	{
+		int words = wordCount;
+		
+		// loop through each character in text
+		for (int i = 0; i < text.Length; i++)
+		{
+			if (text[i] == ' ')
+			{
+				words--;
+			}
+			
+			if (words <= 0)
+			{
+				return text.Substring(0, i);
+			}
+		}
+		
+		return text;
+	}
+}
