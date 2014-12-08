7a8ca89bcff9ae98b6562b06689c985329685f08
Andrew AJ Mandula <ajman@csh.rit.edu>
2014-12-08 16:53:30 -0500

    added new credits, disclaimer, and main menu background

--- UnityFiles/Assets/Scripts/mainMenu.cs
+++ UnityFiles/Assets/Scripts/mainMenu.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class mainMenu : MonoBehaviour {
 
 	public GUIStyle skin;
-	public string credit = "Original Concept by: \n Nikko Williard and Andrew Mandula \n XML Backend provided by: \n Ed Mead \n Scipt written by: \n Andrew Mandula and Nathaniel Case \n Development by: \n Nikko Williard and Andrew Mandula \n Font Asset by G3Typefaces at dafont.com \n Music: \n Jason Weinberger Rendition of Mahler's Symphony No.5 I. Trauermarsh \n Remy Atwork by: \n Jennifer Kotler \n Special Thanks to Remy Decausemaker";
- 
+	public string credit = "Original Concept by: \n Nikko Williard and Andrew Mandula \n Backend provided by: \n Twine2Unity \n Scipt written by: \n Andrew Mandula and Nathaniel Case \n Development by: \n Nikko Williard, Andrew Mandula, Alex Mack \n Font Asset by G3Typefaces at dafont.com \n Music: \n Jason Weinberger Rendition of Mahler's Symphony No.5 I. Trauermarsh \n Remy Atwork by: \n Jennifer Kotler \n Special Thanks to Remy Decausemaker";
+
 	bool ShowCredits;	// Use this for initialization
 	void Start () {
 		
@@ -23,6 +23,9 @@ public class mainMenu : MonoBehaviour {
 		{
 			GUI.Box(new Rect(Screen.width - (Screen.width-20), (Screen.height/4)-5, Screen.width - 10, Screen.height/4), "Remy the Flossopher VS Net Neutrality",skin);
 
+			GUI.Box(new Rect(Screen.width - (Screen.width-20), (Screen.height/3)-5, Screen.width - 10, Screen.height/4), "*Any legal advice is only relevant to fictional video game realities",skin);
+
+
 			if(GUI.Button(new Rect(Screen.width - (Screen.width-20), 2*(Screen.height/4)-5, Screen.width - 10, Screen.height/4), "Play",skin))
 			{
 				if (Application.CanStreamedLevelBeLoaded ("GameGameGame")) 
@@ -36,11 +39,15 @@ public class mainMenu : MonoBehaviour {
 				ShowCredits = true;
 			}
 
-			GUI.Box(new Rect(Screen.width - (Screen.width-20), Screen.height - (Screen.height + 50), Screen.width, Screen.height/4), "*Any legal advice is only relevant to fictional video game realities",skin);
+			GUI.Box(new Rect(Screen.width - (Screen.width-20), Screen.height - (Screen.height + 50), Screen.width, Screen.height/4), "*",skin);
 		}
 		else if(ShowCredits == true)
 		{
 			GUI.Box(new Rect(Screen.width- (Screen.width-20), Screen.height/20, Screen.width- 10, Screen.height-10),credit, skin);
+			if(GUI.Button(new Rect(Screen.width- (Screen.width-20), Screen.height*9/10, Screen.width-10, Screen.height-10),"Back",skin));
+			{
+				ShowCredits = false;
+			}
 		}
 		
 	}
